7106b1b71a86d747a84da06fa43c1e928be5c9fc
Logan Edwards <loganwedwards@gmail.com>
2014-08-02 15:28:10 -0400

    Refactor to use servo in addition to 4 relays
    For an implementation of this project, a 12VDC winch will be used that
    will pull up to 100A, so to simplify the wiring needs, a simple servo
    will be used to toggle a DPDT switch in parallel to the winch remote.
    This will be a safety and cost-savings measure. The code still has 4
    channel relay capabilities. Right now, channel 1 is momentary on type,
    while channels 2-4 are toggle-type.

--- Arduino Sketches/sketch/sketch.ino
+++ Arduino Sketches/sketch/sketch.ino
@@ -1,8 +1,11 @@
+#include <Servo.h>
 // Hardware component with bluetooth control
 //---------------------------------------------------
 // TODO
 // 1. Send feedback on pin state back to client
-// 2. Test char reading
+// 2. Test char reading via BT
+// 3. Logic to handle toggling fwd/rev on motor and
+//    using off position
 //---------------------------------------------------
 // 4 Channel relay
 // 1) + Raise motor
@@ -11,48 +14,81 @@
 // 4) Toggle
 
 int relay1 = 4;
-int relay1Val = 0;
+int relay1Val = 1;
 int relay2 = 5;
-int relay2Val = 0;
+int relay2Val = 1;
 int relay3 = 6;
-int relay3Val = 0;
+int relay3Val = 1;
 int relay4 = 7;
-int relay4Val = 0;
+int relay4Val = 1;
 
-// bluetooth read
+// Servo
+Servo servo;
+int servoPos = 30;
+int servoFWD = 90;
+int servoREV = 30;
+int servoOff = 60;
+
+// serial read
 char val;
 
 // output feedback
 int ledPin = 13;
 
 void setup() {
-  
+  // Relay board
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
   pinMode(relay4, OUTPUT);
   pinMode(ledPin, OUTPUT);
   
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, HIGH);
+  digitalWrite(relay3, HIGH);
+  digitalWrite(relay4, HIGH);
+  
   Serial.begin(9600);
+  
+  // Servo
+  servo.attach(2);
   
 }
 
 void loop () {
-  
+  val = 'X';
   if (Serial.available()) {
     val = Serial.read(); 
   }
   
-  // raise motor
-  if (val == 'U') {
+  if (val == 'F') {
+    servo.write(servoFWD); 
+    delay(100);
+  }
+  else if ( val == 'R') {
+    servo.write(servoREV); 
+    delay(100);
+  }
+  
+  // momentary on for channel 1
+  else if (val == 'A') {
+    digitalWrite(relay1, LOW);
+    delay(250);
     digitalWrite(relay1, HIGH);  
   }
-  // lower motor
-  else if (val == 'D') {
-    digialWrite(relay2, HIGH); 
+  // toggle channel 2
+  else if (val == 'B') {
+    relay2Val = digitalRead(relay2);
+    if (relay2Val == 1) {
+      digitalWrite(relay2, LOW);
+    }
+    else {
+      digitalWrite(relay2, HIGH);
+    }
+    delay(250);
   }
-  // turn channel 3 on
-  else if (val == 'A') {
+  // toggle channel 3
+  else if (val == 'C') {
     relay3Val = digitalRead(relay3);
     if (relay3Val == 1) {
       digitalWrite(relay3, LOW);
@@ -63,8 +99,8 @@ void loop () {
     delay(250);
   }
   
-  // turn channel 4 on
-  else if (val == 'B') {
+  // toggle channel 4
+  else if (val == 'D') {
     relay4Val = digitalRead(relay4);
     if (relay4Val == 1) {
       digitalWrite(relay4, LOW);
@@ -75,7 +111,7 @@ void loop () {
     delay(250); 
   }
   else {
-     delay(1000);
+     delay(100);
   }
   
 }
